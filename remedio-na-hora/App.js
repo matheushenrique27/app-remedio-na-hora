27d734dc213f8810f3b70c4da60f5e17ba24cfb0
vinicius dos santos tavares <vini4812@hotmail.com.br>
2023-06-15 08:33:08 -0300

    Adição de navegação na tela principal.

--- remedio-na-hora/App.js
+++ remedio-na-hora/App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Login } from './views/Login';
+import { Cadastro } from './views/Cadastro';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={Login} options={{ title: 'Login' }} />
+        <Stack.Screen name='Cadastro' component={Cadastro} options={{ title: 'Cadastro' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
