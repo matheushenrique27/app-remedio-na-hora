c2afa30be5b019a7df6d41e0ab9b010bd0ada408
vinicius dos santos tavares <vini4812@hotmail.com.br>
2023-06-15 13:35:33 -0300

    Adição do firebase

---
+++ remedio-na-hora/api/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYYrtIoM9TE8hdf2Q6jjh5dML4K1WSJfY",
+  authDomain: "dm-ufrn.firebaseapp.com",
+  projectId: "dm-ufrn",
+  storageBucket: "dm-ufrn.appspot.com",
+  messagingSenderId: "522169446668",
+  appId: "1:522169446668:web:cea36be4c6df9d6ec11929"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
