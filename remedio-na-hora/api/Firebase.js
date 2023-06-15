4944139a178921a6db892a0841e1eb3f88afacb9
vinicius dos santos tavares <vini4812@hotmail.com.br>
2023-06-15 14:04:02 -0300

    Adição do firebase

--- remedio-na-hora/api/Firebase.js
+++ remedio-na-hora/api/Firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import {firebase} from "firebase/compat/app";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,5 +15,8 @@ const firebaseConfig = {
   appId: "1:522169446668:web:cea36be4c6df9d6ec11929"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+if(!firebase.apps.lenth){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export {firebase};
--- remedio-na-hora/api/userController.js
+++ remedio-na-hora/api/userController.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { firebase } from "./Firebase";
 
 class UserController {
 
